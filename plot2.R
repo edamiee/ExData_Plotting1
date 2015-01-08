0f1f0d4f1d429f9287614dc6a8421fd2a81ff19a
Damien <dame81@Damiens-MBP.att.net>
2015-01-07 23:43:35 -0600

    Updated Changes
    Plot 2

--- plot2.R
+++ plot2.R
@@ -1,2 +1,15 @@
 ## Damien Edwards
-## Course Project 1
+## Course Project 1 Plot 2
+
+## Read data frame and subset the data frame based on dates
+df <-read.table('household_power_consumption.txt',header=TRUE, sep=';',na.strings = "?")
+df.sub1 <- subset(df,as.Date(df$Date,format="%d/%m/%Y") >="2007-02-01"& as.Date(df$Date,format="%d/%m/%Y") <="2007-02-02" )
+df.sub1$Date <- as.POSIXlt(paste(as.Date(d$Date, format="%d/%m/%Y"), d$Time, sep=" "))
+## Create plot
+library(datasets)
+png("plot1.png", width=480, height=480)
+par(mar=c(4.7, 4.7, 0.7, 0.7))
+with(df.sub1,plot(df.sub1$Date,df.sub1$Global_active_power,type="l"))
+title(ylab="Global Active Power (kilowatts)",xlab="")
+
+dev.off()
